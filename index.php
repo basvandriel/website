15b231164c2b3dfbbdd039170f9b60f6c364372f
Bas van Driel <basvandriel94@gmail.com>
2017-01-12 21:27:14 +0100

    Installed bootstrap via composer and configured in index.php

--- index.php
+++ index.php
@@ -3,12 +3,7 @@
 <head>
     <meta charset="UTF-8">
 
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
-    <!-- The bootstrap CSS file    -->
-    <link rel="stylesheet" href="assets/styles/bootstrap.css">
-
-    <!-- Favicon collection for most devices -->
+    <!--Favicon collection for most devices-->
     <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-icon-72x72.png">
@@ -27,25 +22,11 @@
     <meta name="msapplication-TileImage" content="assets/images/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
-    <!-- JavaScript scripts -->
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
-
     <title>Bas van Driel</title>
 </head>
 <body>
-<div class="row">
-    <div class="small-6 columns">
-        <span>Hey, this is me, my name is Bas.</span>
-        <br>
-        <p>I’m a programmer who strives to deliver creative solutions. I am mainly focused in
-            back-end web development and new technologies.</p>
-    </div>
-    <div class="small-6 columns">6 columns</div>
-</div>
-
-
-<img src="assets/images/profile-pictures/bas-pixelated-white.png">
-
-<p>You can send me an email on <a href="mailto:basvandriel94@gmail.com">basvandriel94@gmail.com</a></p>
+<?php
+    echo "basvandriel94@gmail.com";
+?>
 </body>
 </html>
