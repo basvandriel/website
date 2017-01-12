79c01d05326cf0e97dded42debce88f27bcf7e18
Bas van Driel <basvandriel94@gmail.com>
2017-01-12 20:23:19 +0100

    Changed directory structure for test envoirement

---
+++ tests/WWW/Test/PHPUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace WWW\Test;
+
+    class PHPUnitTest extends \PHPUnit_Framework_TestCase
+    {
+        public function testPHPUnit()
+        {
+            self::assertTrue(true);
+        }
+    }
