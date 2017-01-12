dca072ffa4598017f18cb27969cdfda0c9e63ec9
Bas van Driel <basvandriel94@gmail.com>
2017-01-12 21:27:14 +0100

    Installed bootstrap via composer and configured in index.php

--- index.php
+++ index.php
@@ -3,30 +3,49 @@
 <head>
     <meta charset="UTF-8">
 
-    <!--Favicon collection for most devices-->
-    <link rel="apple-touch-icon" sizes="57x57" href="var/public/assets/images/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="var/public/assets/images/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="var/public/assets/images/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="var/public/assets/images/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="var/public/assets/images/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="var/public/assets/images/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="var/public/assets/images/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="var/public/assets/images/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="var/public/assets/images/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="var/public/assets/images/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="var/public/assets/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="var/public/assets/images/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="var/public/assets/images/favicon/favicon-16x16.png">
-    <link rel="manifest" href="var/public/assets/images/favicon/manifest.json">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
+    <!-- The bootstrap CSS file    -->
+    <link rel="stylesheet" href="assets/styles/bootstrap.css">
+
+    <!-- Favicon collection for most devices -->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets/images/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
+    <!-- JavaScript scripts -->
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
     <title>Bas van Driel</title>
 </head>
 <body>
-<?php
-    echo "basvandriel94@gmail.com";
-?>
+<div class="row">
+    <div class="small-6 columns">
+        <span>Hey, this is me, my name is Bas.</span>
+        <br>
+        <p>I’m a programmer who strives to deliver creative solutions. I am mainly focused in
+            back-end web development and new technologies.</p>
+    </div>
+    <div class="small-6 columns">6 columns</div>
+</div>
+
+
+<img src="assets/images/profile-pictures/bas-pixelated-white.png">
+
+<p>You can send me an email on <a href="mailto:basvandriel94@gmail.com">basvandriel94@gmail.com</a></p>
 </body>
 </html>
