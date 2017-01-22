4e8d727c74a80e3f8a79cfd14b8fd5b1920990b9
Bas van Driel <basvandriel94@gmail.com>
2017-01-22 17:22:01 +0100

    Filled in text for first education

--- index.php
+++ index.php
@@ -163,20 +163,32 @@
                                         </td>
                                         <td class="education-details">
                                             <p class="education-details-header">Application and media development</p>
-                                            <p>Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
-                                               zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak
-                                               sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
-                                               en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet
-                                               alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd,
-                                               overgenomen in elektronische letterzetting. Het is in de jaren '60
-                                               populair geworden met de introductie van Letraset vellen met Lorem Ipsum
-                                               passages en meer recentelijk door desktop publishing software zoals Aldus
-                                               PageMaker die versies van Lorem Ipsum bevatten.
+                                            <p>This training is teaching the technical part of
+                                               application
+                                               development. It starts with learning the syntax of the Java programming
+                                               language using small assignments. After that the student learns
+                                               best practices for object-oriented programming by doing real-world
+                                               projects
+                                               within a team of
+                                               other students. In these projects the students also learns to use the
+                                               necessary tools for building the application.</p>
+
+                                            <p>Besides programming the students also learn the essentials of using
+                                               databases and project-management using SCRUM.</p>
+
+                                            <p>
+                                                Within this training, the students have to do 2 internships. These are
+                                                mostly focused on learning the technical part of application
+                                                development In a real-world working environment.
+                                                The first internship
+                                                is 10 weeks and is aimed at orientating at the job itself.
+
+                                                The second internship is for the examination of the student.
                                             </p>
 
                                             <p>
-                                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
-                                                zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
+                                                This training is based on
+                                                level 4 MBO (secondary vocational education).
                                             </p>
 
                                         </td>
@@ -359,7 +371,7 @@
                                             </p>
                                         </td>
                                         <td class="job-details">
-                                            <p class="job-details-header">Service Support</p>
+                                            <p class="job-details-header">Internship, Service Support employee</p>
                                             <p>Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
                                                zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak
                                                sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
@@ -387,7 +399,7 @@
                                             </p>
                                         </td>
                                         <td class="job-details">
-                                            <p class="job-details-header">ICT Servicedesk</p>
+                                            <p class="job-details-header">Internship, ICT Servicedesk</p>
                                             <p>Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
                                                zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak
                                                sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
@@ -443,7 +455,7 @@
                 </div>
                 <div id="skills-container">
                     <div class="container">
-                        
+
                     </div>
                 </div>
 
