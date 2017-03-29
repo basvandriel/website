38c67865b1a5f833781e05f32948e42e6d8736c4
Bas van Driel <basvandriel94@gmail.com>
2017-03-29 21:39:38 +0200

    start of contact container

--- index.php
+++ index.php
@@ -616,7 +616,36 @@
 
     <div id="contact" class="contact-container">
         <div class="container">
-
+            <form class="form-horizontal" role="form" method="post" action="index.php">
+                <div class="form-group">
+                    <label for="name" class="col-sm-2 control-label">Full name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Full name" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message" class="col-sm-2 control-label">Message</label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" rows="4" name="message"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-2">
+                        <! Will be used to display an alert to the user>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </body>
