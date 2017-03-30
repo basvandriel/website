bba9a65e0e445a6c38c444bd95e75af60e58f58c
Bas van Driel <basvandriel94@gmail.com>
2017-03-30 21:12:23 +0200

    format

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,27 @@
 module.exports = function (grunt) {
-
-    // Project configuration.
+    /**
+     * Project configuration
+     */
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js_distribution: {
+                src: ['var/public/bower/jquery/dist/jquery.min.js',
+                        'var/public/bower/bootstrap/dist/js/bootstrap.min.js',
+                        'var/public/bower/particles.js/particles.js',
+                        'var/public/assets/scripts/bootstrap.js'],
+                dest: 'dist/script.js'
+            }
+        }
     });
 
+    /**
+     * Load tasks
+     */
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default tasks
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['concat']);
 
     /**
      * Deploy to a server
