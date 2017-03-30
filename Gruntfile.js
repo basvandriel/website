a45b2d32a5be9f3a3fa542600bbbbf155d176379
Bas van Driel <basvandriel94@gmail.com>
2017-03-30 21:54:26 +0200

    try git deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,22 @@ module.exports = function (grunt) {
         concat: {
             js_distribution: {
                 src: ['var/public/bower/jquery/dist/jquery.min.js',
-                        'var/public/bower/bootstrap/dist/js/bootstrap.min.js',
-                        'var/public/bower/particles.js/particles.js',
-                        'var/public/assets/scripts/bootstrap.js'],
+                    'var/public/bower/bootstrap/dist/js/bootstrap.min.js',
+                    'var/public/bower/particles.js/particles.js',
+                    'var/public/assets/scripts/bootstrap.js'],
                 dest: 'dist/script.js'
+            }
+        },
+
+        /*
+            Deployment
+         */
+        git_deploy: {
+            your_target: {
+                options: {
+                    url: 'https://github.com/basvandriel/basvandriel.nl.git'
+                },
+                src: './dist/'
             }
         }
     });
@@ -19,6 +31,7 @@ module.exports = function (grunt) {
      * Load tasks
      */
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-git-deploy');
 
     // Default tasks
     grunt.registerTask('default', ['concat']);
