d1e0b255bb5403a22614fd35ffb1d2ff51da913f
Bas van Driel <basvandriel94@gmail.com>
2017-06-08 17:22:35 +0200

    Fix a zoom issue

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=1">
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
 
     <!-- JavaScript libraries -->
     <!-- build:js var/public/assets/scripts/libraries.min.js -->
