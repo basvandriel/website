5c5418463e6094c21b272d3bdda2004ce376e7a6
Bas <basvandriel94@gmail.com>
2018-11-29 16:20:28 +0100

    Installed clean webpack plugin

--- etc/build/plugins.js
+++ etc/build/plugins.js
@@ -5,8 +5,19 @@ const path  = require('path'),
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+/**
+ * Paths to clean on build
+ */
+const pathsToClean = [
+    'build'
+];
 
 module.exports = [
+    new CleanWebpackPlugin(pathsToClean, {
+        root: paths.ROOT
+    }),
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: path.resolve(paths.PAGES, 'index.pug'),
