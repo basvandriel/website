416e2634e19c2f9c543eec2e292c7f73e552b043
Bas <basvandriel94@gmail.com>
2019-02-07 23:01:21 +0100

    start of multiple gulp files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,7 @@
-import gulp from 'gulp';
+'use strict';
 
-import "./bin/clean";
+import gulp from 'gulp';
+import Registry from 'gulp-hub';
+
+/* load some gulpfiles into the registry */
+gulp.registry(new Registry(['./bin/*.js']));
