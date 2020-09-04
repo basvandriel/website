85d382922fac676a90563b0e438ac30e5648693b
Bas <bas.vandriel.nl@gmail.com>
2020-09-04 20:34:41 +0200

    Remove out-dated react-reveal package. TODO: Add fades later on 😅

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby'
-import Fade from 'react-reveal/Fade';
+import { Fade } from 'react-awesome-reveal';
 
 import styled from 'styled-components';
 import theme from '../theme';
 
-import logo from '@images/logo_new.png'
+import logo from '@images/logo_new.png';
+
+import 'bootstrap-icons/bootstrap-icons.svg';
 
 const { colors, fontSizes } = theme;
 
@@ -22,6 +24,12 @@ const StyledContainer = styled.header`
     height: 125px;
     width: 100%;
     `;
+
+const StyledHamburger = styled.div`
+    display: none;
+
+    @media (max-width: 768em) {display: flex;}
+`;
 
 const StyledNav = styled.nav`
     display: flex;
@@ -45,36 +53,52 @@ const StyledListLink = styled(Link)`
 `;
 
 class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { menuOpen: false }
+    }
+
+    componentDidMount() {
+        // window.addEventListener('resize', () => {
+        //     this.handleResize();
+        //     console.log(window.innerWidth);
+        // });
+    }
+    toggleMenu = () => this.setState({ menuOpen: !this.state.menuOpen });
+    handleResize = () => (window.innerWidth > 768 && this.state.menuOpen) ? this.toggleMenu() : undefined;
+
     render() {
         return (
             <StyledContainer>
 
                 <StyledNav>
-                    <Fade duration={1000} delay={500} distance="30px">
-
-                        <div className='logo'>
-                            <Link to='/'><img src={logo} alt='' /></Link>
-                        </div>
-                    </Fade>
+                    {/* <Fade duration={1000} delay={500} distance="30px"> */}
+                    <div className='logo'>
+                        <Link to='/'><img src={logo} alt='' /></Link>
+                    </div>
 
                     <ul className="nav justify-content-end align-items-center">
-                        <Fade duration={1000} delay={500} distance="30px">
-
-                            <StyledListItem className='nav-item'>
-                                <StyledListLink to='/#about'>Over mij</StyledListLink>
-                            </StyledListItem>
-                            <StyledListItem className='nav-item'>
-                                <StyledListLink to='/#services'>Diensten</StyledListLink>
-                            </StyledListItem>
-                            {/* <StyledListItem className='nav-item'>
+                        <StyledListItem className='nav-item'>
+                            <StyledListLink to='/#about'>Over mij</StyledListLink>
+                        </StyledListItem>
+                        <StyledListItem className='nav-item'>
+                            <StyledListLink to='/#services'>Diensten</StyledListLink>
+                        </StyledListItem>
+                        {/* <StyledListItem className='nav-item'>
                                 <StyledListLink to='/'>Case studies</StyledListLink>
                             </StyledListItem> */}
-                            <StyledListItem className='nav-item'>
-                                <StyledListLink to='/#contact' variant="outline-primary">Contact</StyledListLink>
-                            </StyledListItem>
-                        </Fade>
+                        <StyledListItem className='nav-item'>
+                            <StyledListLink to='/#contact' variant="outline-primary">Contact</StyledListLink>
+                        </StyledListItem>
                     </ul>
                 </StyledNav>
+
+                <StyledHamburger>
+                    <svg width="40PX" height="40PX" viewBox="0 0 16 16" className="bi bi-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                </StyledHamburger>
 
             </StyledContainer >
         );
